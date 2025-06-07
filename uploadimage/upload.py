839bd4e3c6f2be2e74c8f83abf571c06233b3c83
09amitab <09amitabpradhan@gmail.com>
2025-06-07 19:32:04 +0530

    Initial commit - upload and process lambdas

--- uploadimage/upload.py
+++ uploadimage/upload.py
@@ -42,13 +42,16 @@ def lambda_handler(event, context):
             ContentType=f"image/{ext}"
         )
 
-        return {
-            "statusCode": 200,
-            "headers": {
-                "Access-Control-Allow-Origin": "*"
-            },
-            "body": json.dumps({"message": "Upload successful"})
-        }
+    return {
+        "statusCode": 200,
+        "headers": {
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Headers": "*",
+              "Access-Control-Allow-Methods": "POST,OPTIONS"
+    },
+    "body": json.dumps({"message": "Upload successful"})
+}
+
 
     except Exception as e:
         print("Error:", str(e))
